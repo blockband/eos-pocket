b7bc290a2d204e547ca029b534b5f7fb339ee7db
elon.kwon <elon.kwon@kakaocorp.com>
2018-08-17 21:40:56 +0900

    getInfo 불러오기 버튼.

--- src/stores/eosioStore.js
+++ src/stores/eosioStore.js
@@ -1,13 +1,15 @@
 import { decorate, observable, action } from 'mobx'
 import EosAgent from '../EosAgent'
 
+const eosAgent = new EosAgent()
+
 class EosioStore {
     info = null;
     count = 0;
 
     getInfo = async () => {
         try {
-          const eosInfo = await EosAgent.getInfo()
+          const eosInfo = await eosAgent.getInfo()
     
           this.info = eosInfo
         } catch (e) {}
